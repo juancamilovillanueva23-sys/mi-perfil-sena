fe8a0d18fb3ca26db655c06827255ec9a61d917f
Juan <Juancamilovillanueav23@gmail.com>
2026-09-04 10:20:18 -0500

    ultima version

--- js/aprendices_db.js
+++ js/aprendices_db.js
@@ -33,71 +33,6 @@ const APRENDICES_DATA = [
     "fraseFavorita": "La práctica constante y la disciplina convierten el conocimiento en maestría.",
     "estado": "Guiando la sesión de aprendizaje"
   },
-<<<<<<< HEAD
- {
-  "id": "juan-camilo-villanueva-montoya",
-  "nombreCompleto": "Juan Camilo Villanueva Montoya",
-  "ficha": "3294152",
-  "programa": "Análisis y Desarrollo de Software",
-  "rol": "Aprendiz / Desarrollador Junior",
-  "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación.",
-  "habilidades": [
-    "Git",
-    "GitHub",
-    "HTML5",
-    "CSS3",
-    "JavaScript"
-  ],
-  "intereses": [
-    "Desarrollo Web",
-    "Programación"
-  ],
-  "avatar": "assets/avatares/avatar_default.svg",
-  "redes": {
-    "github": "https://github.com/juancamilovillanueva23-sys",
-    "linkedin": "https://linkedin.com/in/juancamilovillanueva23-sys",
-    "portafolio": "https://juancamilovillanueva23-sys.github.io/mi-perfil-sena"
-  },
-  "fraseFavorita": "Hazlo simple, mantenlo limpio.",
-  "estado": "Listo para colaborar"
-},
-  {
-    "id": "Valentinapardo",
-    "nombreCompleto": "Valentina Chaves",
-    "ficha": "3294152",
-    "programa": "Análisis y Desarrollo de Software",
-    "rol": "Aprendiz / Desarrollador Junior",
-    "biografia": "Estudiante en Analisis y Desarrollo de Software en el sena,apasionada por la tecnologia y la progrmacion mas orientada al backend con conocimientos basicos en JavaScript,python,php,laravel .",
-    "habilidades": [
-      "postgresql",
-      "GitHub",
-      "HTML5",
-      "CSS3",
-      "JavaScript",
-      "Trabajo en Equipo"
-    ],
-    "intereses": [
-      "Desarrollo backend",
-      "Bases de Datos",
-      "Inteligencia Artificial"
-    ],
-    "avatar": "assets/avatares/avatar_laura.svg",
-    "redes": {
-      "github": "https://github.com/Valentinna-p28",
-      "linkedin": "https://linkedin.com/in/tu-usuario",
-      "portafolio": "https://tu-usuario.github.io/mi-perfil-sena"
-    },
-    "fraseFavorita": "El código limpio siempre parece que fue escrito por alguien a quien le importaba.",
-    "estado": "Disponible para proyectos colaborativos"
-  },
-  {
-    "id": "kerlon-velasquez",
-    "nombreCompleto": "Kerlon Velasquez",
-    "ficha": "3294152",
-    "programa": "Análisis y Desarrollo de Software",
-    "rol": "Aprendiz / Desarrollador Junior",
-    "biografia": "Aprendiz SENA apasionado por el desarrollo web.",
-=======
   {
     "id": "Valentinapardo",
     "nombreCompleto": "Valentina Chaves",
@@ -215,89 +150,6 @@ const APRENDICES_DATA = [
     "programa": "Análisis y Desarrollo de Software",
     "rol": "Aprendiz / Desarrollador Junior",
     "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación en constante aprendizaje.",
->>>>>>> 757f5c15cd0dbf0b9b29267035ec659b591aa358
-    "habilidades": [
-      "Git",
-      "GitHub",
-      "HTML5",
-      "CSS3",
-      "JavaScript"
-    ],
-    "intereses": [
-      "Desarrollo Web",
-      "Programación"
-    ],
-    "avatar": "assets/avatares/avatar_carlos.svg",
-    "redes": {
-<<<<<<< HEAD
-      "github": "https://github.com/kerlonvelasquez525-star",
-      "linkedin": "https://www.linkedin.com/in/kerlon-velasquez-72180b429/",
-      "portafolio": "https://github.com/kerlonvelasquez525-star/crear_componentes_frontend"
-    },
-    "fraseFavorita": "No eres un dios... ¡pero aun así me deleitaré con tu esencia!",
-    "estado": "Listo para colaborar"
-  },
-  {
-    "id": "yojhan-felipe-forero-trivino",
-    "nombreCompleto": "Yojhan Felipe Forero Triviño",
-    "ficha": "3294152",
-    "programa": "Análisis y Desarrollo de Software",
-    "rol": "Aprendiz / Desarrollador Junior",
-    "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación. Futuro programador senior.",
-    "habilidades": [
-      "Git",
-      "GitHub",
-      "HTML5",
-      "CSS3",
-      "JavaScript"
-    ],
-    "intereses": [
-      "Desarrollo Web",
-      "Programación"
-    ],
-    "avatar": "assets/avatares/avatar_default.svg",
-    "redes": {
-      "github": "https://github.com/yojhandev",
-      "linkedin": "https://linkedin.com/in/yojhandev",
-      "portafolio": "https://yojhandev.github.io/mi-perfil-sena"
-    },
-    "fraseFavorita": "Sin mente que pensar. Sin voluntad que quebrar. Sin voz que clame por el sufrimiento. Nacido de Dios y del Vacío. Tú sellarás la luz cegadora que devora sus sueños. Tú eres el Recipiente. Tú eres el Hollow Knight.",
-    "estado": "Listo para colaborar"
-  },
-  {
-    "id": "andres-martinez",
-    "nombreCompleto": "Andres Martinez",
-    "ficha": "3294152",
-    "programa": "Análisis y Desarrollo de Software",
-    "rol": "Aprendiz / Desarrollador Junior",
-    "biografia": "Aprendiz SENA interesado en aprender cosas nuevas y trabajar en equipo",
-    "habilidades": [
-      "Git",
-      "GitHub",
-      "HTML5",
-      "CSS3",
-      "JavaScript"
-    ],
-    "intereses": [
-      "Desarrollo Web",
-      "Programación"
-    ],
-    "avatar": "assets/avatares/avatar_carlos.svg",
-    "redes": {
-      "github": "https://github.com/andresdevapps",
-      "linkedin": "https://linkedin.com/in/andresdevapps",
-      "portafolio": "https://andresdevapps.github.io/mi-perfil-sena"
-    },
-    "fraseFavorita": "Mejor intentar y fallar que no intentar.",
-    "estado": "Listo para colaborar"
-  },
-  {
-    "id": "cristian-david-lemus-ovalle",
-    "nombreCompleto": "Cristian David Lemus Ovalle",
-    "ficha": "3294152",
-    "programa": "Análisis y Desarrollo de Software",
-    "rol": "Aprendiz / Desarrollador Junior",
-    "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación en constante aprendizaje.",
     "habilidades": [
       "Git",
       "GitHub",
@@ -319,16 +171,6 @@ const APRENDICES_DATA = [
     "estado": "Listo para colaborar"
   },
   {
-=======
-      "github": "https://github.com/Anfixsinax",
-      "linkedin": "https://linkedin.com/in/Anfixsinax",
-      "portafolio": "https://Anfixsinax.github.io/mi-perfil-sena"
-    },
-    "fraseFavorita": "Mejor intentar y fallar que no intentar.",
-    "estado": "Listo para colaborar"
-  },
-  {
->>>>>>> 757f5c15cd0dbf0b9b29267035ec659b591aa358
     "id": "angel-quintana",
     "nombreCompleto": "Angel Quintana",
     "ficha": "3294152",
@@ -381,10 +223,6 @@ const APRENDICES_DATA = [
     },
     "fraseFavorita": "Yo solía ser un desarrollador como tú, pero entonces recibí un virus en el ordenador.",
     "estado": "Listo para colaborar"
-<<<<<<< HEAD
-  }];
- 
-=======
   },
   {
   "id": "william-stevan-torres-zapata",
@@ -584,13 +422,8 @@ const APRENDICES_DATA = [
 }
 
 ];
->>>>>>> 757f5c15cd0dbf0b9b29267035ec659b591aa358
 
 // Si se ejecuta en entorno Node/módulos
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = APRENDICES_DATA;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 757f5c15cd0dbf0b9b29267035ec659b591aa358
